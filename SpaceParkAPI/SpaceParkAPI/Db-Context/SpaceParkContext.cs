644243cf701fa4a7554c7a6228cac73e11a621e4
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-10-05 15:07:31 +0200

    Boyscouting: Removed unused using directives in classes, makes for a cleaner code :)

--- SpaceParkAPI/SpaceParkAPI/Db-Context/SpaceParkContext.cs
+++ SpaceParkAPI/SpaceParkAPI/Db-Context/SpaceParkContext.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using SpaceParkAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SpaceParkAPI.Db_Context
 {
